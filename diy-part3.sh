92358c0e5c422b2e4e34e36ef2dccb355a5a92ac
hza81007155 <800755@qq.com>
2026-08-23 08:09:34 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -21,6 +21,7 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-passwall2
 rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/luci/applications/luci-app-lucky
 rm -rf feeds/luci/applications/luci-app-wol
 rm -rf feeds/luci/applications/luci-app-vlmcsd
